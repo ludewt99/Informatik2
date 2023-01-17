520cf5365002e40899d1ff0916567e14397304f0
ludewt99 <luisdeuschle@gmail.com>
2023-01-17 17:03:22 +0100

    Server Socket added

---
+++ ServerSocket1/ServerSocket1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace ServerSocket1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener serverSocket = null;
+
+            try
+            {
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+                serverSocket = new TcpListener(localAddr, 8080);
+                serverSocket.Start();
+
+                while (true)
+                {
+                    Console.WriteLine("Warte auf Verbindung...");
+
+                    TcpClient client = serverSocket.AcceptTcpClient();
+                    Console.WriteLine("Verbindung hergestellt");
+
+                    NetworkStream stream = client.GetStream();
+
+
+                    Byte[] daten = new Byte[256];
+
+                    // Nachricht empfangen
+                    int i = stream.Read(daten, 0, daten.Length);
+                    Console.WriteLine("Die Nachricht hat {0} bytes", i);
+
+                    string RecNachricht = Encoding.ASCII.GetString(daten, 0, i);
+                    Console.WriteLine("Empfangene Nachricht: " + RecNachricht);
+
+                    //Serveranfragen
+                    switch(RecNachricht)
+                    {
+                        case "Online?":
+                            {
+                                daten = Encoding.ASCII.GetBytes("Server ist online");
+                                stream.Write(daten, 0, daten.Length);
+                                break;
+                            }
+
+                        case "Info":
+                            {
+                                daten = Encoding.ASCII.GetBytes("Server-IP: 127.0.0.1 , Port: 8080");
+                                stream.Write(daten, 0, daten.Length);
+                                break;
+                            }
+
+                        case "Matrikelnummer":
+                            {
+                                daten = Encoding.ASCII.GetBytes("Matr. Nr.: 767440");
+                                stream.Write(daten, 0, daten.Length);
+                                break;
+                            }
+                        
+                        default:
+                            {
+                                daten = Encoding.ASCII.GetBytes("Anfrage kann nicht verarbeitet werden");
+                                stream.Write(daten, 0, daten.Length);
+                                break;
+                            }
+                            
+                    }
+                    
+                   
+                }
+            }
+            catch(ArgumentException e1)
+            {
+                Console.WriteLine(e1.Message);
+            }
+            catch(IOException e2)
+            {
+                Console.WriteLine(e2.Message);
+            }
+        }
+    }
+}
